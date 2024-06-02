7dfc51706df503aebd9a19df81e32078f3bc0a5d
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-06-02 22:43:51 +0100

    app tsx reformatted

--- To-Do-list/src/App.tsx
+++ To-Do-list/src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import Todo from "./components/Todo.tsx"
+import Todo from "./components/Todo.tsx";
 
 function App() {
   return (
- 
-      <div>
-        <Todo/>
-      </div>
-      
-
+    <div>
+      <Todo />
+    </div>
   );
 }
 
